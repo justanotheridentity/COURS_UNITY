d9a9021ebb45fe6eece52e3acb55adac001a6500
JustanotherIdentity <justanotheridentity12@gmail.com>
2018-12-18 15:53:26 +0100

    SemaineIntensive

---
+++ Assets/Semain Intensive/ARC_Script/BlockGO.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGO : MonoBehaviour {
+
+    public float speed;
+    public float numberAssign;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //speed = Random.Range(1.0f, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+
+    {
+        transform.position += new Vector3(0.0f, 0.0f, -(speed * Time.fixedDeltaTime) )  ;
+	}
+
+    public void GoFast ()
+    {
+
+        StartCoroutine(GoFaster());
+
+    }
+
+    IEnumerator GoFaster()
+    {
+        transform.position += new Vector3(0.0f, 0.0f, -(speed*3 * Time.fixedDeltaTime));
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(GoFaster());
+    }
+
+    public void SetNumber (int numberToSet)
+    {
+        numberAssign = numberToSet;
+    }
+
+}
---
+++ Assets/Semain Intensive/ARC_Script/Recommence.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recommence : MonoBehaviour {
+
+    public int [] PositionDesCubes;
+    
+    private string myString;
+
+    private string aAfficher;
+   
+
+    private string numero;
+
+    public GameObject depart;
+
+    public int nbCube;
+
+    //private int[] tableau;
+
+    List<int> listNumber = new List<int>();
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.gameObject != null)
+        {
+            if (other.transform.parent.gameObject.name == "Ligne") // other =
+            {
+                other.gameObject.transform.position = new Vector3(depart.gameObject.transform.position.x, depart.gameObject.transform.position.y, depart.gameObject.transform.position.z);
+
+                //other.gameObject.GetComponent<BlockGO>().GoFast();
+
+                //On executé la fonction avance rapide de l'objet.
+            }
+        }
+
+        // On prend un cliché du tableau
+        
+        numero = other.gameObject.tag;
+        Debug.Log(numero);
+        int numeroGameObject = int.Parse(numero);
+
+        
+
+
+
+        for (int i=1; i<nbCube+1;i++)
+        {
+            listNumber.Add(i);
+        }
+        
+
+
+        Cliche(numeroGameObject);
+            
+
+        
+        aAfficher = "";
+
+        for (int b= 0; b<10 ; b =b+1)
+        {
+             myString= PositionDesCubes[b].ToString();
+            
+            aAfficher = aAfficher + "-" + myString; ;
+
+
+        }
+        Debug.Log(aAfficher );
+
+        aAfficher = "";
+        for (int b = 0; b < 10; b = b + 1)
+        {
+            myString = listNumber[b].ToString();
+
+            aAfficher = aAfficher + "-" + myString; ;
+
+
+        }
+        Debug.Log(aAfficher);
+
+    }
+
+
+    private void Cliche (int numeroGO)
+    {
+        int caseNumber = numeroGO - 1;
+        for (int i=0; i<nbCube;i++)
+        {
+            PositionDesCubes[i] = listNumber [caseNumber] ;
+            //Debug.Log(tableau[caseNumber]);
+            if(caseNumber==nbCube-1)
+            {
+                caseNumber = 0;
+            }
+            else
+            {
+                caseNumber++;
+            }
+        }
+    }
+}
